1bea580040ff1066ca7a08653ab41f6434bf05ca
Hardik Ghai <hardikghai224@gmail.com>
2025-09-16 03:56:54 +0530

    Update Q3.cpp

--- Assignment 2/Q3.cpp
+++ Assignment 2/Q3.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
 using namespace std;
-
-int missinglinear(int arr[], int size){
-    for(int i=0;i<size;i++){
-        if(arr[i] != i+1){
-            return i+1;
-        }
-    }
-    return size + 1;
-}
-
 int missingbinary(int arr[], int size){
     int left = 0;
     int right = size-1;
@@ -27,7 +17,7 @@ int missingbinary(int arr[], int size){
 int main(){
     int arr[] = {1,2,3,4,5,6,8,9};
     int size = sizeof(arr) / sizeof(arr[0]);
-    missinglinear(arr, size);
     missingbinary(arr, size);
     return 0;
 }
+
