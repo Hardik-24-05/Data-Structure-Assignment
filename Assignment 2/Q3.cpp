83cd3254b02c2e7b9bbf5694c1430f20e1216dfe
Hardik Ghai <hardikghai224@gmail.com>
2025-09-16 01:13:25 +0530

    Update Q3.cpp

--- Assignment 2/Q3.cpp
+++ Assignment 2/Q3.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int arr[8] = {1,2,3,4,5,6,7,9};
-    int maxi = -3;
-    int sum2=0;
-    int sum1=0;
-    for(int i=0;i<8;i++){
-        if(arr[i]>maxi){
-            maxi=arr[i];
+int missinglinear(int arr[], int size){
+    for(int i=0;i<size;i++){
+        if(arr[i] != i+1){
+            return i+1;
         }
     }
-    cout<<maxi<<endl;
-    for(int i=0;i<=maxi;i++){
-        sum1+=i;
+    return size + 1;
+}
+
+int missingbinary(int arr[], int size){
+    int left = 0;
+    int right = size-1;
+
+    while(left<=right){
+        int mid = left + (right - left) / 2;
+        if(arr[mid] == mid+1){
+            left = mid + 1;
+        }
+        else right = mid - 1;
     }
-    cout<<sum1<<endl;
-    for(int i=0;i<8;i++){
-        sum2+=arr[i];
-    }
-    cout<<sum2<<endl;
-    cout<<sum1-sum2;
+    return left + 1;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,8,9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    missinglinear(arr, size);
+    missingbinary(arr, size);
+    return 0;
 }
